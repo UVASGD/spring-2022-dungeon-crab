9a5add8d7a96e6bc990c844f44ff0e230c132699
ns5ub <ns5ub@virginia.edu>
2022-04-17 19:27:38 -0400

    button + grate basic script

--- Dungeon Crab/Assets/buttonScript.cs
+++ Dungeon Crab/Assets/buttonScript.cs
@@ -5,21 +5,40 @@ using UnityEngine;
 public class buttonScript : MonoBehaviour
 {
     public bool isActive;
-    List<GameObject> thingsOnButton;
+    public int numThingsOnButton = 0;
+    public ParentGrate grate;
+    //List<GameObject> thingsOnButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(numThingsOnButton);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        //thingsOnButton.Add(other.gameObject);
+        numThingsOnButton++;
+        if (numThingsOnButton > 0) {
+            grate.open();
+        }
+        Debug.Log(numThingsOnButton);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        numThingsOnButton--;
+        if (numThingsOnButton == 0)
+        {
+            grate.close();
+        }
+
+        Debug.Log(numThingsOnButton);
     }
 }
